170e565c0e75f5afa54d640864e8e8665de86aa1
verissimo-sn <jose.neto@minu.co>
2022-02-05 07:39:22 -0300

    feat(user): add uuid

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,17 @@
+import { v4 as uuid } from 'uuid';
+
 export class User {
-  readonly id: number;
+  readonly id?: string;
 
   readonly name: string;
 
   readonly email: string;
 
   readonly password: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
