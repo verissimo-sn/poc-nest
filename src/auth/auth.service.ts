df4aac321b2e3c8099d7e6dae3de476502094e3a
verissimo-sn <jose.neto@minu.co>
2022-02-05 07:43:56 -0300

    feat(auth): add decrypt password in to validate login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import { compareSync } from 'bcrypt';
+import { User } from 'src/users/entities/user.entity';
 
 export interface ILoginResponse {
   access_token: string;
@@ -14,15 +16,28 @@ export class AuthService {
   ) {}
 
   async login(email: string, password: string): Promise<ILoginResponse> {
+    const user = this.validate(email, password);
+
+    const payload = { sub: user.id, username: user.name };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  validate(email: string, password: string): User {
     const user = this.usersService.findByEmail(email);
 
-    if (user && user.password === password) {
-      const payload = { sub: user.id, username: user.name };
-      return {
-        access_token: this.jwtService.sign(payload),
-      };
+    if (!user) {
+      throw new Error('User or password incorrect');
     }
 
-    return null;
+    const PasswordMatch = compareSync(password, user.password);
+
+    if (!PasswordMatch) {
+      throw new Error('User or password incorrect');
+    }
+
+    return user;
   }
 }
