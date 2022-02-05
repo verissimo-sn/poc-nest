6afaec9d551e887e1e46fb76f3e46dfb6696217f
verissimo-sn <jose.neto@minu.co>
2022-02-05 06:12:13 -0300

    refactor: remove moduiles folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './modules/tasks/tasks.module';
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TasksModule, UsersModule, AuthModule],
