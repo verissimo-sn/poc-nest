e1707a2c092ef88824151955f5a29aa244c8ac55
verissimo-sn <jose.neto@minu.co>
2022-02-07 18:33:15 -0300

    chore(db): add mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { dbConfig } from './database/dbConfig';
 
 @Module({
-  imports: [TasksModule, UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(dbConfig.mongo_uri, {
+      useNewUrlParser: true,
+    }),
+    TasksModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/database/dbConfig.ts
@@ -0,0 +1,3 @@
+export const dbConfig = {
+  mongo_uri: 'mongodb://localhost:27017/nestTasks',
+};
