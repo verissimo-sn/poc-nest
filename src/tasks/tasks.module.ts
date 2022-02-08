995973d7194c25a4383f19526c6d60722e17f453
verissimo-sn <jose.neto@minu.co>
2022-02-08 09:42:58 -0300

    feat(task): add schemas in task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from './entities/task.entity';
+import { User, UserSchema } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Task.name, schema: TaskSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
